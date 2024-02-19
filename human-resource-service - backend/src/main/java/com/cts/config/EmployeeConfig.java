e0fa989bd9f7f7d4bb6dc36cbcaae11141a13850
Saiprasuna-24 <87110531+Saiprasuna-24@users.noreply.github.com>
2024-02-19 18:50:14 +0530

    Update EmployeeConfig.java

--- human-resource-service - backend/src/main/java/com/cts/config/EmployeeConfig.java
+++ human-resource-service - backend/src/main/java/com/cts/config/EmployeeConfig.java
@@ -1,14 +1,6 @@
 package com.cts.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.*;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spi.service.contexts.SecurityContext;
-//import springfox.documentation.spring.web.plugins.Docket;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -23,28 +15,7 @@ import java.util.Collections;
 import java.util.List;
 @Configuration
 public class EmployeeConfig {
-//
-//    private ApiInfo apiInfo(){
-//        return new ApiInfo(
-//                "Spring Boot Blog REST APIs",
-//                "Spring Boot Blog REST API Documentation",
-//                "1",
-//                "Terms of service",
-//                new Contact("Saradindu", "", ""),
-//                "License of API",
-//                "API license URL"
-//        );
-//    }
-//
-//    @Bean
-//    public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
+  
 	 public static final String AUTHORIZATION_HEADER = "Authorization";
 
 	    private ApiKey apiKey(){
